347d81d0cdb0bea58c1e5e6730fa868850b156ee
eugene wu <sirrice@gmail.com>
2015-05-01 21:47:31 -0700

    rm deps on header and footer

--- products.php
+++ products.php
@@ -1,7 +1,35 @@
-<?php
-  session_start();
-  include_once("./include/header.inc");
-?>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="./include/style.css" />
+	</head>
+
+<body>
+
+
+<table class="header" ><tr>
+	<td style="width: 0%">
+		<a href="./index.php">
+			<img src="./docs/logo_small.png" />
+		</a>
+	</td>
+	<td style="width: 100%">
+	
+<div class="nav">
+	<a href="./index.php">Home</a>
+	<a href="./products.php">Products</a>
+	<a href="./presentations.php">Presentations</a>
+	<a href="./about.php">About Us</a>
+	<a href="./contact.php">Contact Us</a>
+	<!--<a href="./partners.php">Partners</a>-->
+</div>
+<div class="title">	
+	<h1 style="margin-bottom: 0px;">Sino-American Pigment Systems, Inc.</h1>
+	<h4>High-Performance, Non-Toxic, Hybrid Pigments</h4>
+</div>
+</td>
+</tr></table>
+
+<div class="main">
 
 <h2>Our Products</h2>
 <script src="http://static.cloudstitch.io/cloudstitch.js" app="sirrice/sino-american-pigment-systems"></script>
@@ -13,6 +41,15 @@
 
 <h3>Note, Production Status Key: C = Customized Production, S = Standard Production</h3>
 
-<?php
-  include_once("./include/footer.inc")
-?>
+</div>
+
+<div class="footer">
+&copy; 2007 Sino American Pigment Systems, Inc. <br/>
+1620 Norvell St.
+El Cerrito, CA
+94530
+</div>
+
+
+</body>
+</html>
